c8d3eca0596f089290c56ab7a3ff143ed9ed04b5
LiaCabrall <lianav.cabral@gmail.com>
2025-05-15 21:42:49 -0300

    teste

--- API/index.js
+++ API/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Liana é gay');
+  res.send('Arthur é chato');
 });
 
 const PORT = process.env.PORT || 3000;
